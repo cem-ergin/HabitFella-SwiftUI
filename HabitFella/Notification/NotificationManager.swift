b3ac61751ec63c7e8035db0ee437c8605024d9be
Cem Ergin <cem-ergin@outlook.com>
2022-10-09 23:35:53 +0100

    Add: notification manager

---
+++ HabitFella/Notification/NotificationManager.swift
@@ -0,0 +1,61 @@
+//
+//  NotificationManager.swift
+//  HabitFella
+//
+//  Created by Cem Ergin on 09/10/2022.
+//
+
+import Foundation
+import UserNotifications
+
+final class NotificationManager: ObservableObject {
+    @Published private(set) var notifications: [UNNotificationRequest] = []
+    @Published private(set) var authorizationStatus: UNAuthorizationStatus?
+    
+    let center = UNUserNotificationCenter.current()
+    
+    func reloadAuthorizationStatus() {
+        center.getNotificationSettings { settings in
+            DispatchQueue.main.async {
+                self.authorizationStatus = settings.authorizationStatus
+            }
+        }
+    }
+    
+    func requestAuthorization() {
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { isGranted, _ in
+            DispatchQueue.main.async {
+                self.authorizationStatus = isGranted ? .authorized : .denied
+            }
+        }
+    }
+    
+    func reloadLocalNotifications() {
+        center.getPendingNotificationRequests { notifications in
+            DispatchQueue.main.async {
+                self.notifications = notifications
+            }
+        }
+    }
+    
+    func createLocalNotification(title: String, body: String, hour: Int, minute: Int, completion: @escaping (Error?) -> Void) {
+        var dateComponents = DateComponents()
+        dateComponents.hour = hour
+        dateComponents.minute = minute
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.title = title
+        notificationContent.sound = .default
+        notificationContent.body = body
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: notificationContent, trigger: trigger)
+        
+        center.add(request, withCompletionHandler: completion)
+    }
+    
+    func deleteLocalNotifications(identifiers: [String]) {
+        center.removePendingNotificationRequests(withIdentifiers: identifiers)
+    }
+}
